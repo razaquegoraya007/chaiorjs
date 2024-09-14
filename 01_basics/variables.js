d53e5e70907c590576e70c24be45660c0a84b3c3
Razaque Goraya <arazaque.goraya@gmail.com>
2024-09-14 13:28:26 +0500

    Mesage

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const  accountID = 5945
+
+let accountName = "Razaque"
+
+// var accountEmail = "razaqueg007@gmail.com"
+// var  is not used now a days because it is deprecated we use let nowadays
+// prefer not to use var because of issue in block scope and functional scope
+
+// accountID = 34 not allowed to change the value of const
+
+
+
+
+console.table([accountID,accountEmail,accountName])
