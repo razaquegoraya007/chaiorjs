149143c05da5d748bc01c28b07504699dc2fcdfe
Razaque Goraya <arazaque.goraya@gmail.com>
2024-09-14 14:50:19 +0500

    b

--- 01_basics/index.js
+++ 01_basics/index.js
@@ -1,2 +1,6 @@
-// console.log("Abdul Razaque")
+// "use strict";
+// treat all JS Code as newer version of JS , now a days basically it takes automatically newer version
 
+//orginal documentation of JS is found at
+//https://tc39.es/ecma262/
+// MDN Javascript also used for documentation
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,20 +1,23 @@
-const  accountID = 5945
-
-let accountName = "Razaque"
-
+// const  accountID = 5945
+// let accountName = "Razaque"
 // accountCity = "Mehrabpur"
 // this is also possible but we not prefer to use this also
-
-var accountEmail = "razaqueg007@gmail.com"
+// var accountEmail = "razaqueg007@gmail.com"
 // var  is not used now a days because it is deprecated we use let nowadays
 // prefer not to use var because of issue in block scope and functional scope
-
 // accountID = 34 not allowed to change the value of const
-
-console.table([accountID,accountName])
+// console.table([accountID,accountName])
 // we can print the different variable in table format by using console.table([variableNames])
 
 
 
 // Next part (Data Types and ECMA Script)
+// alert("Hello")
+// we are using node js not browser so it will not open here
+
+// variable number is range upto 2^53
+// then we use bigInt
+
+// null is standalone value
+console.log(typeof null)
 
