4fa0ede7504b864809a1a700a4268c7b42618503
Razaque Goraya <arazaque.goraya@gmail.com>
2024-09-14 13:34:25 +0500

    a

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,13 +2,19 @@ const  accountID = 5945
 
 let accountName = "Razaque"
 
-// var accountEmail = "razaqueg007@gmail.com"
+// accountCity = "Mehrabpur"
+// this is also possible but we not prefer to use this also
+
+var accountEmail = "razaqueg007@gmail.com"
 // var  is not used now a days because it is deprecated we use let nowadays
 // prefer not to use var because of issue in block scope and functional scope
 
 // accountID = 34 not allowed to change the value of const
 
+console.table([accountID,accountName])
+// we can print the different variable in table format by using console.table([variableNames])
 
 
 
-console.table([accountID,accountEmail,accountName])
+// Next part (Data Types and ECMA Script)
+
